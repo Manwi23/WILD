b071e7ddebe54b3ee3d1170eca7a20a27fb2b944
Katarzyna Miernikiewicz <kasia.mie@interia.pl>
2021-01-07 22:04:22 +0100

    converter added

---
+++ converter.py
@@ -0,0 +1,19 @@
+# units converter
+
+def f2c(fahrenheit):
+    return (fahrenheit - 32)*5/9
+
+def c2f(celsius):
+    return celsius*9/5 + 32
+
+def mph2ms(mph):
+    return mph * 0.44704
+
+def ms2mph(ms):
+    return ms / 0.44704
+
+def in2mm(inch):
+    return inch * 25.4
+
+def mm2in(mm):
+    return mm / 25.4
