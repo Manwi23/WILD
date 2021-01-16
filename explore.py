c317718364bbfadf29f05c32e10fbe60d03101d7
Katarzyna Miernikiewicz <kasia.mie@interia.pl>
2021-01-16 23:43:17 +0100

    some xgboost fun

--- explore.py
+++ explore.py
@@ -9,16 +9,13 @@ import math
 def cel(far):
     return (far - 32) * 5.0/9.0
 
-
-dfs = []
-
-vals = []
-
 def process_df(df, label):
 
     rename_dict={}
     for c in df.columns:
         rename_dict[c] = c.split("|")[0]
+
+    vals = []
 
     df = df.rename(columns=rename_dict)
     df["Temperature"] = df["Temperature"].apply(cel)
@@ -29,130 +26,132 @@ def process_df(df, label):
     vals.append(vals_for_year[:89])
     return df
 
-if exists("processed.csv"):
-    result_df = pd.read_csv("processed.csv")
-else:
-    for y in [2014,2015,2016,2017,2018,2019,2020]:
-        dfs.append(process_df(pd.read_csv(f"scraped/wroclaw{y}-01-01{y}-03-31.csv"), str(y)))
+def read_or_process_data():
+    if exists("processed.csv"):
+        result_df = pd.read_csv("processed.csv")
+    else:
+        dfs = []
+        for y in [2014,2015,2016,2017,2018,2019,2020]:
+            dfs.append(process_df(pd.read_csv(f"scraped/wroclaw{y}-01-01{y}-03-31.csv"), str(y)))
 
-    # plt.plot(np.mean(np.array(vals),axis=0), label="avg", linewidth=5.0)
+        # plt.plot(np.mean(np.array(vals),axis=0), label="avg", linewidth=5.0)
 
-    # plt.legend()
-    # plt.show()
+        # plt.legend()
+        # plt.show()
 
-    train_data = []
-    test_data = []
+        out = []
 
-    out = []
+        for df in dfs:
+            rows = list(df.iterrows())
+            length = len(rows)
+            print(length)
 
-    for df in dfs:
-        rows = list(df.iterrows())
-        length = len(rows)
-        print(length)
+            for i in range(length):
+                if i + 48 >= length:
+                    continue
 
-        for i in range(length):
-            if i + 48 >= length:
-                continue
+                _, prev = rows[i]
+                _, curr = rows[i+24]
+                _, next_ = rows[i+48]
 
-            _, prev = rows[i]
-            _, curr = rows[i+24]
-            _, next_ = rows[i+48]
+                # print(prev["Time"],curr["Time"],next["Time"])
+                # print(next_)
+                target = next_["Temperature"] - prev["Temperature"]
+                curr["TemperaturePrev"] = prev["Temperature"]
+                curr["target"] = target
+                out.append(curr)
 
-            # print(prev["Time"],curr["Time"],next["Time"])
-            # print(next_)
-            target = next_["Temperature"] - prev["Temperature"]
-            curr["TemperaturePrev"] = prev["Temperature"]
-            curr["target"] = target
-            out.append(curr)
+        result_df = pd.DataFrame(out).reset_index()
+        result_df.to_csv("processed.csv")
+    return result_df
 
-    result_df = pd.DataFrame(out).reset_index()
-    result_df.to_csv("processed.csv")
+if __name__ == "__main__":
+    result_df = read_or_process_data()
+    result_df, test_df = train_test_split(result_df, test_size = 0.14285714285, shuffle=False)
 
-result_df, test_df = train_test_split(result_df, test_size = 0.14285714285, shuffle=False)
+    def low(data, confidence=0.95):
+        a = 1.0 * np.array(data)
+        n = len(a)
+        m, se = np.mean(a), scipy.stats.sem(a)
+        h = se * scipy.stats.t.ppf((1 + confidence) / 2., n-1)
+        return m-h
 
-def low(data, confidence=0.95):
-    a = 1.0 * np.array(data)
-    n = len(a)
-    m, se = np.mean(a), scipy.stats.sem(a)
-    h = se * scipy.stats.t.ppf((1 + confidence) / 2., n-1)
-    return m-h
+    def high(data, confidence=0.95):
+        a = 1.0 * np.array(data)
+        n = len(a)
+        m, se = np.mean(a), scipy.stats.sem(a)
+        h = se * scipy.stats.t.ppf((1 + confidence) / 2., n-1)
+        return m+h
 
-def high(data, confidence=0.95):
-    a = 1.0 * np.array(data)
-    n = len(a)
-    m, se = np.mean(a), scipy.stats.sem(a)
-    h = se * scipy.stats.t.ppf((1 + confidence) / 2., n-1)
-    return m+h
+    names = ["mean", "95%% low", "95%% high", "values count"]
 
-names = ["mean", "95%% low", "95%% high", "values count"]
-
-def calculate_stats(column):
-    gb = result_df.groupby(column)["target"]
-    val = pd.concat([gb.mean().rename("mean"),
-                     gb.apply(low).rename("95% low"),
-                     gb.apply(high).rename("95% high"), 
-                     gb.count().rename("count")], axis=1).sort_values("count",ascending=False)
-    
-    print(val)
-    return val
-
-
-
-print(result_df.head())
-wind = calculate_stats("Wind")
-cond = calculate_stats("Condition")
-time = calculate_stats("Time")
-
-cross = pd.crosstab(result_df["Wind"], result_df["Condition"])
-
-s = cross.sum()
-c_reduced = cross[s.sort_values(ascending=False).index[:8]]
-
-print(c_reduced)
+    def calculate_stats(column):
+        gb = result_df.groupby(column)["target"]
+        val = pd.concat([gb.mean().rename("mean"),
+                        gb.apply(low).rename("95% low"),
+                        gb.apply(high).rename("95% high"), 
+                        gb.count().rename("count")], axis=1).sort_values("count",ascending=False)
+        
+        print(val)
+        return val
 
 
-cross2 = pd.crosstab(result_df["Wind"], result_df["Condition"],values=result_df["target"], aggfunc="mean")
-# cross2 = pd.crosstab(result_df["Wind"], result_df["Condition"],values=result_df["target"], aggfunc=(lambda x: (low(x), high(x))))
-cross2cut = cross2[s.sort_values(ascending=False).index[:8]]
-print(cross2cut)
 
-test_df_mean = test_df["target"].mean()
-# print(test_df_mean)
+    print(result_df.head())
+    wind = calculate_stats("Wind")
+    cond = calculate_stats("Condition")
+    time = calculate_stats("Time")
 
-def calc_score(func, df):
-    v = 0
-    d = 0
-    for i, row in df.iterrows():
-        val = (row["target"] - func(row))**2
-        base = (row["target"] - test_df_mean)**2
-        if math.isnan(val):
-            v+=base
-        else:
-            v+=val
-        d += base
+    cross = pd.crosstab(result_df["Wind"], result_df["Condition"])
 
-    print(1 - v/d)
+    s = cross.sum()
+    c_reduced = cross[s.sort_values(ascending=False).index[:8]]
 
-def cond_calc(row):
-    # print(row["Wind"])
-    return cond.loc[row["Condition"], "mean"]
+    print(c_reduced)
 
-def wind_calc(row):
-    # print(row["Wind"])
-    return wind.loc[row["Wind"], "mean"]
 
-def wind_cond_calc(row):
-    # print(row["Wind"])
-    return cross2.loc[row["Wind"], row["Condition"]]
+    cross2 = pd.crosstab(result_df["Wind"], result_df["Condition"],values=result_df["target"], aggfunc="mean")
+    # cross2 = pd.crosstab(result_df["Wind"], result_df["Condition"],values=result_df["target"], aggfunc=(lambda x: (low(x), high(x))))
+    cross2cut = cross2[s.sort_values(ascending=False).index[:8]]
+    print(cross2cut)
 
-print("wind only")
-calc_score(wind_calc, result_df)
-calc_score(wind_calc, test_df)
+    test_df_mean = test_df["target"].mean()
+    # print(test_df_mean)
 
-print("wind and condition cross-table")
-calc_score(wind_cond_calc, result_df)
-calc_score(wind_cond_calc, test_df)
+    def calc_score(func, df):
+        v = 0
+        d = 0
+        for i, row in df.iterrows():
+            val = (row["target"] - func(row))**2
+            base = (row["target"] - test_df_mean)**2
+            if math.isnan(val):
+                v+=base
+            else:
+                v+=val
+            d += base
 
-print("condition only")
-calc_score(cond_calc, result_df)
-calc_score(cond_calc, test_df)
+        print(1 - v/d)
+
+    def cond_calc(row):
+        # print(row["Wind"])
+        return cond.loc[row["Condition"], "mean"]
+
+    def wind_calc(row):
+        # print(row["Wind"])
+        return wind.loc[row["Wind"], "mean"]
+
+    def wind_cond_calc(row):
+        # print(row["Wind"])
+        return cross2.loc[row["Wind"], row["Condition"]]
+
+    print("wind only")
+    calc_score(wind_calc, result_df)
+    calc_score(wind_calc, test_df)
+
+    print("wind and condition cross-table")
+    calc_score(wind_cond_calc, result_df)
+    calc_score(wind_cond_calc, test_df)
+
+    print("condition only")
+    calc_score(cond_calc, result_df)
+    calc_score(cond_calc, test_df)
---
+++ trees.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+import scipy.stats as sstats
+from os.path import exists
+from sklearn.model_selection import train_test_split
+import math
+from sklearn.cluster import KMeans
+import xgboost as xgb
+
+from explore import read_or_process_data
+
+def apply_pearson(target, column):
+    r, p = sstats.pearsonr(target, column)
+    # print(r, p, what)
+    return p
+
+def apply_chi2(target_grouped, column):
+    cross = pd.crosstab(target_grouped, column)
+    stat,p,dof,expec = sstats.chi2_contingency(cross)
+    return p
+
+def group_column(column, name):
+    kmeans = KMeans(n_clusters=20).fit(np.array(column).reshape(-1,1))
+    return pd.DataFrame({name: list(map(lambda x: str(x), kmeans.labels_))})
+
+def calc_score(true_vals, predicted, mean):
+    v = 0
+    d = 0
+    for t, p in zip(true_vals, predicted):
+        val = (t - p)**2
+        base = (t - mean)**2
+        if math.isnan(val):
+            v+=base
+        else:
+            v+=val
+        d += base
+
+    print(1 - v/d)
+
+results_df = read_or_process_data()
+
+
+ps = []
+
+target = results_df['target']
+target_grouped = group_column(target, 'target_g')
+# print(target_grouped)
+forbidden = ['target', 'index', 'Unnamed: 0', 'Date', 'Time']
+
+# print(results_df.dtypes['target'])
+
+res_data = results_df.copy()
+
+for i in results_df.columns:
+    if i not in forbidden:
+        if results_df.dtypes[i] != 'object':
+            # print(i, results_df.dtypes[i])
+            # print(i)
+            p = apply_pearson(target, results_df[i])
+            ps += [(i,p)]
+        else:
+            # print(i)
+            # col = group_column(results_df[i])
+            p = apply_chi2(target_grouped['target_g'], results_df[i])
+            ps += [(i,p)]
+            res_data = res_data.drop(columns=[i])
+        # res_data += [results_df[i]]
+    else:
+        # print(i, 'dropped')
+        res_data = res_data.drop(columns=[i])
+
+# res_data = pd.DataFrame(res_data)
+# print(res_data)
+
+# print(ps)
+for i in ps:
+    print(i)
+
+res_data['target'] = target
+train_df, test_df = train_test_split(res_data, test_size = 0.14285714285, shuffle=False)
+
+xgb_model = xgb.XGBRegressor(objective="reg:squarederror")
+
+target_train = train_df['target']
+train_df = train_df.drop(columns=['target'])
+target_test = test_df.copy()
+test_df = test_df.drop(columns=['target'])
+
+xgb_model.fit(train_df, target_train)
+
+ytarget = xgb_model.predict(test_df)
+mm = target_test['target'].mean()
+
+calc_score(target_test['target'], ytarget, mm)
+
+
+ytarget = xgb_model.predict(train_df)
+mm = target_train.mean()
+
+calc_score(target_train, ytarget, mm)
